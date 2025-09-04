0da86701b1bc24c5c7464ee5e04bc8d5dda0a6a7
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-04 23:38:31 +0900

    노트 정보 진입시점 분리

--- quiz-works-app/src/features/noteInfo/flashcardInfo.tsx
+++ quiz-works-app/src/features/noteInfo/flashcardInfo.tsx
@@ -1,35 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from 'shared/ui/button/button'
-import { privateApi } from 'shared/api/api';
 import { MongoFlashcard } from 'entities/flashcard/note';
-import { useParams } from 'react-router-dom';
 import s from './flashcardInfo.module.css'
 
-export const FlashCardInfo: React.FC = () => {
+interface FlashcardInfoProps {
+  flashcard: MongoFlashcard;
+}
 
-  const { id } = useParams<{ id: string }>();
-  const [note, setNote] = useState<MongoFlashcard | null>(null);
+export const FlashCardInfo: React.FC<FlashcardInfoProps> = ({flashcard}) => {
+
+  const [note, setNote] = useState<MongoFlashcard>(flashcard);
   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    if (!id) {
-      setLoading(false);
-      return;
-    }
-    const getNoteById = async () => {
-      try {
-        setLoading(true);
-        const res = await privateApi.get(`/notes/typing/${id}`);
-        setNote(res.data)
-      } catch (error) {
-        alert('초기 로딩 실패');
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getNoteById();
-  }, [id]);
 
   if (loading) {
     <div>로딩중임다</div>
