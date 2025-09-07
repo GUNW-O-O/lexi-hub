8bb21a5bfb917ec0d26c99f2181be6d92f515c12
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-07 21:29:40 +0900

    로고, 헤더 이름 추가

--- quiz-works-app/src/widgets/header/header.tsx
+++ quiz-works-app/src/widgets/header/header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './header.module.css'
 import { Button } from 'shared/ui/button/button'
 import { useAuth } from 'shared/lib/context/authProvider'
+import { Link } from 'react-router-dom'
 
 export const Header: React.FC = () => {
 
@@ -10,7 +11,11 @@ export const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       <div className="logo">
-        <Button children='미정이' to='/' />
+        {/* <Button children='미정이' to='/' /> */}
+        <Link to={'/'} style={{display: 'flex', alignItems: 'center'}}>
+          <img src="/logo.png" alt="LexiHub" />
+          <h2>LexiHub</h2>
+        </Link>
       </div>
       <div className={styles.util}>
         {user ? (
